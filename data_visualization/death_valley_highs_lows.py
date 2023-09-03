09fe0bc7d7c858adc95eabc5198b237c60d5428a
alllexxx1 <alekkorpachyov@gmail.com>
2023-09-03 14:57:35 +0300

    Add displaying weather data in Death Valley the US and add exception handling in case of missing data

--- data_visualization/death_valley_highs_lows.py
+++ data_visualization/death_valley_highs_lows.py
@@ -9,19 +9,23 @@ def convert_to_celsius(fahr_temp: str) -> int:
 
 
 filepath = ('/home/aleksei/projects/data_visualization'
-            '/data/sitka_weather_2021_simple.csv')
+            '/data/death_valley_2021_simple.csv')
 with open(filepath) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     highs, dates, lows = [], [], []
     for row in reader:
-        high = convert_to_celsius(row[4])
-        highs.append(high)
-        date = datetime.strptime(row[2], '%Y-%m-%d')
-        dates.append(date)
-        low = convert_to_celsius(row[5])
-        lows.append(low)
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        try:
+            high = convert_to_celsius(row[4])
+            low = convert_to_celsius(row[5])
+        except ValueError:
+            print(f'Missing data for {current_date}')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
@@ -29,10 +33,12 @@ ax.plot(dates, highs, c='red', alpha=0.5)
 plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
-plt.title('Daily high and low temperatures - 2021', fontsize=24)
+title = 'Daily high and low temperatures - 2018\nDeath Valley, CA'
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (C)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
+
